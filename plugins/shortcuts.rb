3d8d6122efd5153a8694392b71cba389b0bfe341
Jak Spalding <jak.spalding@gmail.com>
2012-09-01 23:17:14 +0100

    adding shortcuts plugin, as well as sqlite db to the bot

---
+++ plugins/shortcuts.rb
@@ -0,0 +1,48 @@
+require 'cinch'
+require 'data_mapper'
+
+class Shortcut
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :command, String, unique: true
+	property :contents, String
+	property :created_by, String
+end
+
+class Shortcuts
+	include Cinch::Plugin
+
+	match /set (.+) (.+)/, method: :add_shortcut
+	match /\?(.+)/, method: :get_shortcut, use_prefix: false
+
+	def set(m, command, contents)
+		begin
+			shortcut = Shortcut.first(:command.like => command.downcase.strip)
+			if shortcut.nil?
+				shortcut = Shortcut.new(
+					:command => command.downcase.strip
+				)
+			end
+			shortcut.contents = contents
+			shortcut.created_by = m.user.nick
+			shortcut.save
+			m.reply "Saved, use ?#{command} to retrieve"
+		rescue => error
+			m.reply "Uh-oh spaghetti-o's!"
+		end
+	end
+
+	def get(m, command)
+		begin
+			shortcut = Shortcut.first(:command.like => command.downcase.strip)
+			if shortcut.nil?
+				m.reply "Nothing found. Add something using '!set #{command.strip} useful stuff here'"
+			else
+				m.reply shortcut.contents
+			end
+		rescue => error
+			m.reply "Uh-oh spaghetti-o's!"
+		end
+	end
+end
--- redpanda.rb
+++ redpanda.rb
@@ -1,3 +1,5 @@
+DBFILE      = "/home/jak/bot/redpanda/sqlite.db"
+
 require 'cinch'
 require 'data_mapper'
 require_relative 'plugins/tube.rb'
@@ -5,23 +7,40 @@ require_relative 'plugins/karma.rb'
 require_relative 'plugins/iplayer.rb'
 require_relative 'plugins/hudsonbots.rb'
 require_relative 'plugins/joinpart.rb'
+require_relative 'plugins/help.rb'
+require_relative 'plugins/shortcuts.rb'
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'sqlite:///home/jak/bot/redpanda/sqlite.db')
+DataMapper.setup(:default, 'sqlite:///' + DBFILE)
+
+# If database doesn't exist, create. Else update
+if File.exists?(DBFILE)
+	DataMapper.auto_upgrade!
+else
+	DataMapper.auto_migrate!
+end
 
 bot = Cinch::Bot.new do
-  configure do |c|
-    c.server = 'irc.dev.bbc.co.uk'
-    c.port = '6697'
-    c.nick = 'redpanda'
-    c.name = 'redpanda'
-    c.realname = 'iPlayer IRC Bot - Jak Spalding'
-    c.channels = ['#iplayer']
-    c.plugins.plugins = [TubeStatus, Karma, Iplayer, HudsonBots, JoinPart]
-    c.ssl.use = true
-    c.ssl.verify = false
-    c.ssl.client_cert = '/home/jak/certs/dev.bbc.co.uk.pem'
-  end
+	configure do |c|
+		c.server = 'irc.dev.bbc.co.uk'
+		c.port = '6697'
+		c.nick = 'redpanda'
+		c.name = 'redpanda'
+		c.realname = 'iPlayer IRC Bot - Jak Spalding'
+		c.channels = ['#iplayer']
+		c.plugins.plugins = [
+			TubeStatus, 
+			Karma, 
+			Iplayer, 
+			HudsonBots, 
+			JoinPart, 
+			Help, 
+			Shortcuts
+		]
+		c.ssl.use = true
+		c.ssl.verify = false
+		c.ssl.client_cert = '/home/jak/certs/dev.bbc.co.uk.pem'
+	end
 end
 
 bot.start
