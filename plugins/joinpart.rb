62ce684a3c97f09f0c58f7a15efb902836494778
Jak Spalding <jak.spalding@gmail.com>
2012-09-01 21:43:38 +0100

    make joining and parting a bit more talkative

--- plugins/joinpart.rb
+++ plugins/joinpart.rb
@@ -1,17 +1,21 @@
 require 'cinch'
 
 class JoinPart
-  include Cinch::Plugin
+	include Cinch::Plugin
 
-  match /join (.+)/, method: :join
-  match /part(?: (.+))?/, method: :part
+	match /join (.+)/, method: :join
+	match /part(?: (.+))?/, method: :part
 
-  def join(m, channel)
-    Channel(channel).join
-  end
+	def join(m, channel)
+		m.reply "Joining #{channel} at the request of #{m.user.nick}"
+		c = Channel(channel)
+		c.join
+		c.msg "Joined at the request of #{m.user.nick}"
+	end
 
-  def part(m, channel)
-    channel ||= m.channel
-    Channel(channel).part if channel
-  end
+	def part(m, channel)
+		channel ||= m.channel
+		m.reply "Leaving #{channel} as the request of #{m.user.nick}"
+		Channel(channel).part if channel
+	end
 end
