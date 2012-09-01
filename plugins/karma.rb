683713ef404ef10f80245e022c580b83baa85627
Jak Spalding <jak.spalding@gmail.com>
2012-09-01 12:58:56 +0100

    shoulda been a class var

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -5,7 +5,7 @@ require 'json'
 class Karma
   include Cinch::Plugin
 
-  @filepath = '/home/jak/bots/redpanda/karma.marshal'
+  @@filepath = '/home/jak/bots/redpanda/karma.marshal'
 
   match /([\w]+)\+\+/, method: :add_karma, use_prefix: false
   match /([\w]+)--/, method: :remove_karma, use_prefix: false
@@ -27,21 +27,21 @@ class Karma
   end
 
   def load_hash
-    if File.exists?(@filepath)
-      File.open(@filepath) do |f|
+    if File.exists?(@@filepath)
+      File.open(@@filepath) do |f|
         @karma = Marshal.load(f)
       end
     else
       @karma = Hash.new
-      File.new(@filepath, 'w')
-      File.open(@filepath, 'w+') do |f|
+      File.new(@@filepath, 'w')
+      File.open(@@filepath, 'w+') do |f|
         Marshal.dump(@karma, f)
       end
     end
   end
 
   def save_hash
-    File.open(@filepath, 'w+') do |f|
+    File.open(@@filepath, 'w+') do |f|
       Marshal.dump(@karma, f)
     end
   end
