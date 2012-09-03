a15646f614d7ca077cb71ed84caeda205cb43328
Jak Spalding <jak.spalding@gmail.com>
2012-09-03 18:36:51 +0100

    making JIRA ticket regex a little less generic

--- plugins/jira.rb
+++ plugins/jira.rb
@@ -4,8 +4,8 @@ require 'mechanize'
 class Jira
 	include Cinch::Plugin
 
-	match /([A-Za-z]+-[0-9]+)/, method: :get_issue, use_prefix: false
-	match /([4-9][0-9]{3})/, method: :get_iplayer_issue, use_prefix: false
+	match /[ ^]([A-Za-z]+-[0-9]+)[ $]/, method: :get_issue, use_prefix: false
+	match /[ ^]([4-9][0-9]{3})[ $]/, method: :get_iplayer_issue, use_prefix: false
 
 	def get_iplayer_issue(m, issue_number)
 		get_issue m, "IPLAYER-#{issue_number}"
@@ -15,7 +15,7 @@ class Jira
 		agent = Mechanize.new
 		agent.cert = CERTIFICATE_PATH
 		agent.key = CERTIFICATE_PATH
-		url = "https://jira.dev.bbc.co.uk/browse/#{issue}"
+		url = "https://" << JIRA_HOST << "/browse/#{issue}"
 		title = agent.get(url).title
 		m.reply "#{title} - [ #{url}  ]"
 	end
