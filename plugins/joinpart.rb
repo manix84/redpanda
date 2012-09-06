35076bb653f7d1c676b7d0e9f5016ac4d999925b
Jak Spalding <jak.spalding@gmail.com>
2012-09-06 16:28:14 +0100

    joinpart plugin: refactor, lose the messages

--- plugins/joinpart.rb
+++ plugins/joinpart.rb
@@ -11,16 +11,12 @@ class JoinPart
 	def join(m, channel)
 		return unless is_admin?(m.user)
 		channel = "##{channel}" unless channel[0,1] == "#"
-		m.reply "Joining #{channel} at the request of #{m.user.nick}"
-		c = Channel(channel)
-		c.join
-		c.msg "Joined at the request of #{m.user.nick}"
+		Channel(channel).join
 	end
 
 	def part(m, channel)
 		return unless is_admin?(m.user)
 		channel ||= m.channel
-		m.reply "Leaving #{channel} as the request of #{m.user.nick}"
 		Channel(channel).part("coz #{m.user.nick} told me too :(") if channel
 	end
 
