049e6de26a96175ccdd8e5707f912efd33eacbb6
Jak Spalding <jak.spalding@gmail.com>
2013-01-29 14:58:24 +0000

    adding a !jira command

--- plugins/jira.rb
+++ plugins/jira.rb
@@ -4,20 +4,38 @@ require 'mechanize'
 class Jira
 	include Cinch::Plugin
 
-	match /(?:[\s,:\?\(]|^)([A-Za-z]+-[0-9]+)(?:[\)\s,:\?]|$)/, method: :get_issue, use_prefix: false
-	match /(?:\s|^)(1[0-9]{4})(?:\s|$)/, method: :get_iplayer_issue, use_prefix: false
+	match /([A-Za-z]+-[0-9]+)/, method: :get_issue, use_prefix: false
+	match /(?:\s|^)([0-9]{4,5})(?:\s|$)/, method: :get_iplayer_issue, use_prefix: false
+	match /jira ([a-z]+-[0-9]+)/i, method: :get_jira_issue
 
 	def get_iplayer_issue(m, issue_number)
-		get_issue m, "IPLAYER-#{issue_number}" if m.channel == '#iplayer'
+		return if m.channel != '#iplayer'
+		issue = issue_title("IPLAYER-#{issue_number}")
+		m.reply issue if not issue.empty?
 	end
 
-	def get_issue(m, issue)
+	def get_issue(m, issue_number)
+		issue = issue_title(issue_number)
+		m.reply issue if not issue.empty?
+	end
+
+	def get_jira_issue(m, issue_number)
+		issue = issue_title(issue_number)
+		if issue.empty?
+			m.reply "That ticket doesn't appear to exist!"
+		else
+			m.reply issue
+		end
+	end
+
+	def issue_title(issue)
 		agent = Mechanize.new
 		agent.cert = CERTIFICATE_PATH
 		agent.key = CERTIFICATE_PATH
 		url = "https://" << JIRA_HOST << "/browse/#{issue}"
 		title = agent.get(url).title
-		m.reply "#{title} - [ #{url}  ]"
+		return "" if title.start_with? "ISSUE DOES NOT EXIST"
+		return "#{title} - [ #{url} ]"
 	end
 
 end
