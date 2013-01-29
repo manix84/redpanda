b47278c7da1d518bef05db1e53c5b2cbdb1da349
Jak Spalding <jak.spalding@gmail.com>
2013-01-29 14:37:02 +0000

    update tube plugin

--- plugins/tube.rb
+++ plugins/tube.rb
@@ -1,19 +1,23 @@
 require 'rest-client'
-require 'cinch'
 require 'json'
+require 'htmlentities'
 
 class TubeStatus
-  include Cinch::Plugin
 
-  match /tube/
+  match /tube$/i
+  match /tube (\w+)$/i
 
-  def execute(m)
-    response = JSON.parse(RestClient.get 'http://api.tubeupdates.com/?method=get.status&lines=all')
+  def execute(m, stations='all')
+    url = "http://api.tubeupdates.com/?method=get.status&lines=#{stations}"
+    response = JSON.parse(RestClient.get url)
     parsed = response['response']
-    if response['response']['lines']
-      response['response']['lines'].each do |line|
-        m.reply "#{line['name']} - #{line['status']}\n"
+    if response['response'] && response['response']['lines']
+      response['response']['lines'].group_by { |line| line['status'] }.each do |status, lines|
+       status = "#{status} - #{lines.map { |l| l['name'] }.join(', ')}"
+       m.reply HTMLEntities.new.decode(status)
       end
+    elsif response['response'] && response['response']['error']
+      m.reply "Error: #{response['response']['error']}"
     else
       m.reply 'Epic tube fail'
     end
