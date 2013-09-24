822fcdfc0c8cdc50c7ae90162f5000185cfddc3d
Matthew Williams <matthew.williams@bbc.co.uk>
2013-09-24 14:50:38 +0100

    Send reply to channel, remove hash from regex match

--- plugins/pull_request.rb
+++ plugins/pull_request.rb
@@ -3,13 +3,13 @@ require 'cinch'
 class Jira
     include Cinch::Plugin
 
-    match(/\W(#\d+)\W/, method: :pull_request_url)
+    match(/\W#(\d+)\W/, method: :pull_request_url)
 
     def pull_request_url(m, pr_number)
         # Need to limit this to #iplayer as it hardcodes the URL
         return if m.channel != '#iplayer'
         url = "https://github.com/iplayer/responsive-web/pull/#{pr_number}"
-        return url
+        m.reply url
     end
 
 end
